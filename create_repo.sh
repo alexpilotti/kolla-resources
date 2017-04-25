2932c1345b1bbaecbee63dfb095ae5c01bcf0b67
danardelean <ardeleandanflorin@gmail.com>
2017-04-25 23:02:31 +0300

    Add tag var

--- create_repo.sh
+++ create_repo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 REPO=$1
-DOCKER_USERNAME=
-DOCKER_PASSWORD=
+# Dockerhub credentials
+DOCKER_USERNAME=""
+DOCKER_PASSWORD=""
 
 echo Creating: $REPO
 curl -s -H "X-Docker-Token: true" -H "Content-Type: application/json"  -X PUT --user $DOCKER_USERNAME:$DOCKER_PASSWORD https://index.docker.io/v1/repositories/$DOCKER_USERNAME/$REPO/ -d '[{"id": "1234"}]'
--- push_all_repos.sh
+++ push_all_repos.sh
@@ -2,5 +2,6 @@
 set -e
 
 DOCKER_NAMESPACE="kolla"
-NEW_NAMESPACE="dardelean"
-for repo in $(sudo docker images --format '{{.Repository}}' | grep $DOCKER_NAMESPACE ); do repo=`echo $repo | awk -F "/" '{print $2}'` && sudo docker tag kolla/$repo:4.0.0 $NEW_NAMESPACE/$repo:4.0.0 && sudo docker push $NEW_NAMESPACE/$repo; done
+NEW_NAMESPACE=""
+TAG="4.0.0"
+for repo in $(sudo docker images --format '{{.Repository}}' | grep $DOCKER_NAMESPACE ); do repo=`echo $repo | awk -F "/" '{print $2}'` && sudo docker tag kolla/$repo:$TAG $NEW_NAMESPACE/$repo:$TAG && sudo docker push $NEW_NAMESPACE/$repo; done
